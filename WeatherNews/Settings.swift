5ae3648635027d2b7916ea49ff94985f90724be3
MohammedHussien10 <86535395+MohammedHussien10@users.noreply.github.com>
2025-10-21 20:16:38 +0300

    WeatherNews

---
+++ WeatherNews/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  WeatherNews
+//
+//  Created by Macos on 21/10/2025.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
