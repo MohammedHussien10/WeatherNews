ebb42dbac40ab3386099e7ce04e5792e69fa9052
MohammedHussien10 <86535395+MohammedHussien10@users.noreply.github.com>
2026-01-15 23:33:19 +0200

    update Ui

---
+++ WeatherNews/PresentationLayer/SplashScreen/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  WeatherNews
+//
+//  Created by Macos on 15/01/2026.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
